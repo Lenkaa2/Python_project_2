c75f091a8fcec9bf34bc362b95bcdf5b3528c943
Lenkaa2 <lenka.pazourova@seznam.cz>
2023-10-30 20:43:57 +0100

    Add more functions and new genereted random number

--- Project_2_bulls_and_cows.py
+++ Project_2_bulls_and_cows.py
@@ -9,22 +9,27 @@ discord: LenkaP
 import random
 import datetime
 
-separator = 50 * "-"
+# Funkce kontrolující vstup uživatele
+def check_len(user_number):
+    """Zkontroluj zadání uživatele a pokud nemá 4 znaky, vrať True, jinak False."""
+    if len(user_number) != 4:
+           return True
+    else:
+        return False
 
-# Úvodní pozdrav
-print("Hi there!",
-      separator,
-      "I've generated a random 4 digit number for you.",
-      "Let's play a bulls and cows game.",
-      separator,
-      sep="\n")
+def check_zero(user_number):
+    """Zkontroluj zadání uživatele a pokud začíná nulou, vrať True, jinak False."""
+    if user_number[0] == '0':
+        return True
+    else:
+        return False
 
-# Generování náhodného 4 ciferného čísla
-secret_number = [random.randint(1,9)] # range(1, 9) --> aby náhodné číslo nezačínalo nulouu
-while len(secret_number) != 4:
-    one_number = random.randint(0, 9)
-    if one_number not in secret_number: # Číslice musí být unikátní
-        secret_number.append(one_number)
+def check_numeric(user_number):
+    """Zkontroluj zadání uživatele a pokud obsahuje nenumerické znaky, vrať True, jinak False"""
+    if not user_number.isnumeric():
+        return True
+    else:
+        return False
 
 def check_duplicate(user_number):
     """ Zkontroluj zadání uživatele a pokud obsahuje duplicity, vrať True, jinak vrať False."""
@@ -35,6 +40,23 @@ def check_duplicate(user_number):
     else: 
         return False
 
+# Generování náhodného čísla
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+while True:
+    secret_number = random.sample(numbers, 4)
+    if secret_number[0] != 0:
+        break
+
+separator = 50 * "-"
+
+# Úvodní pozdrav
+print("Hi there!",
+      separator,
+      "I've generated a random 4 digit number for you.",
+      "Let's play a bulls and cows game.",
+      separator,
+      sep="\n")
+
 attempt = 0 # Počítání pokusů uživatele
 
 start_time = datetime.datetime.now()
@@ -42,11 +64,11 @@ start_time = datetime.datetime.now()
 while True:
     enter_number = input("Enter a number: ")
     print(separator)
-    if len(enter_number) != 4:
+    if check_len(enter_number):
         print("You did not enter a 4 digit number. Try again.")
-    elif enter_number[0] == '0':
+    elif check_zero(enter_number):
         print("Number must not start with zero. Try again.")
-    elif not enter_number.isnumeric():
+    elif check_numeric(enter_number):
         print("Number contains not numeric character. Try again.")
     elif check_duplicate(enter_number):
         print("You entered duplicated values. Try again.")
